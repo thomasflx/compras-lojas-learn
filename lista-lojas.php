8e879715db32804099a3fadd8029f2a52be227a4
Thomas Ewerton <ewerton.thomas@gmail.com>
2021-11-25 20:59:00 -0300

    Cria os arquivos iniciais

---
+++ lista-lojas.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<?php
+	include('includes/head.php');
+	?>
+	
+	<style>
+	
+	</style>
+</head>
+
+<body>
+
+	<h1>Lista de lojas</h1>
+
+</body>
+</html>
---
+++ lista-produtos-loja.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<?php
+	include('includes/head.php');
+	?>
+	
+	<style>
+	
+	</style>
+</head>
+
+<body>
+
+	<h1>Lista de produtos da loja X</h1>
+
+</body>
+</html>
