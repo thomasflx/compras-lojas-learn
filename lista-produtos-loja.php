291471d71bfdbf3003b5a318006e3d3aaa262d0e
Rayssa Fonseca <fonsecarayss14@gmail.com>
2022-02-10 20:19:53 -0300

    lista de produtos da loja, atualizado

--- lista-produtos-loja.php
+++ lista-produtos-loja.php
@@ -12,22 +12,23 @@
 	</style>
 </head>
 
-<body>
+<body style="background-color:lightgreen;">
 <?php
 include('includes/header.php');
 ?>
+
+
 
 	<h1>Lista de produtos da loja</h1>
 	
 	<h2>Escolha o setor da sua compra:</h2>
 	
 	
-	
 	<p><a href="http://localhost/compras-lojas-learn/setor-a%C3%A7ougue.php">Açougue</a></p>
 	<p><a href="http://localhost/compras-lojas-learn/setor-limpeza.php">Limpeza</a></p>
 	<p><a href= "http://localhost/compras-lojas-learn/setor-padaria.php">Padaria</a></p>
-	<p><a href= "http://localhost/compras-lojas-learn/setor-hortali%C3%A7as">Hortaliças</a></p>
-	<p>Cosméticos<em>/</em>Perfumaria</p>
+	<p><a href= "http://localhost/compras-lojas-learn/setor-hortali%C3%A7as.php">Hortaliças</a></p>
+	<p><a href= "http://localhost/compras-lojas-learn/setor-cosmeticos.php">Cosméticos<em>/</em>Perfumaria</a href></p>
 	
 	
 	
