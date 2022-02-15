28989e1b04fc73970591d288d51aacee82b43dc9
Rayssa Fonseca <fonsecarayss14@gmail.com>
2022-02-14 21:59:03 -0300

    img e fundo

---
+++ setor-hortaliças.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>	
+<head>
+	<?php
+	include ('includes/head.php');
+	?>
+	</head>
+	<body style="background-color:beige;">
+	
+	<h2>Escolha seus produtos e adicione no carrinho!</h2>
+	<p>Alface</p>
+	<p>Acelga</p>
+	
+	</body>
+</html>
+
---
+++ setor-limpeza.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php
+	include ('includes/head.php');
+	?>
+	</head>
+	<body style="background-color:beige;">
+	
+	<h2>Escolha seus produtos e adicione no carrinho!</h2>
+	
+	<img src="assets/img/produtos-de-limpeza-para-desenhos-animados-em-casa_18591-52030.webp" alt="produtos de limpeza">
+	
+	<ul>
+	<li>Água Sanitária</li>
+	<li>Amaciante</li>
+	<li>Detergente</li>
+	<li>Sabão Líquido</li>
+	<li>Sabão em Pó</li>
+	<li>Rodo, vassoura, pá</li>
+	<li>Desengordurante</li>
+	<li>Álcool</li>
+	<li>Desinfetante</li>
+	<li>Sacos de lixo</li>
+	<li>Palha de aço</li>
+	<li>Esponja para louça</li>
+	<li>Panos de limpeza e flanelas</li>
+	</ul>
+	
+	</body>
+
+
+
+
+
+
+
+
+
+
+
+
+</head>
+
+</html>
