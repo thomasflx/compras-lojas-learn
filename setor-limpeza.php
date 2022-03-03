00a11dea11836fc30ee43d595fe42f99c2e98f28
Abner <abnerm.arruda@gmail.com>
2022-03-03 12:55:42 -0300

    resolvi o problema dos links

--- setor-limpeza.php
+++ setor-limpeza.php
@@ -1,31 +1,31 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<?php
-	include ('includes/head.php');
-	?>
+	<head>
+		<?php
+		include ('includes/head.php');
+		?>
 	</head>
 	<body style="background-color:beige;">
-	
-	<h2>Escolha seus produtos e adicione no carrinho!</h2>
-	
-	<img src="assets/img/economizar-gastos-produtos-limpeza.jpg" alt="produtos de limpeza">
-	
-	<ul>
-	<li>Água Sanitária</li>
-	<li>Amaciante</li>
-	<li>Detergente</li>
-	<li>Sabão Líquido</li>
-	<li>Sabão em Pó</li>
-	<li>Rodo, vassoura, pá</li>
-	<li>Desengordurante</li>
-	<li>Álcool</li>
-	<li>Desinfetante</li>
-	<li>Sacos de lixo</li>
-	<li>Palha de aço</li>
-	<li>Esponja para louça</li>
-	<li>Panos de limpeza e flanelas</li>
-	</ul>
+		
+		<h2>Escolha seus produtos e adicione no carrinho!</h2>
+		
+		<img src="assets/img/economizar-gastos-produtos-limpeza.jpg" alt="produtos de limpeza">
+		
+		<ul>
+		<li>Água Sanitária</li>
+		<li>Amaciante</li>
+		<li>Detergente</li>
+		<li>Sabão Líquido</li>
+		<li>Sabão em Pó</li>
+		<li>Rodo, vassoura, pá</li>
+		<li>Desengordurante</li>
+		<li>Álcool</li>
+		<li>Desinfetante</li>
+		<li>Sacos de lixo</li>
+		<li>Palha de aço</li>
+		<li>Esponja para louça</li>
+		<li>Panos de limpeza e flanelas</li>
+		</ul>
 	
 	</body>
 
