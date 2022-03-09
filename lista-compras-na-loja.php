0676dbbc5c74517fd71651fd9d0768395793d544
Abner <abnerm.arruda@gmail.com>
2022-03-09 16:10:59 -0300

    fiz o modelo agora só falta algumas coisinhas(algumas das algumas vou precisar de muita ajauda pra entnder  ;) mas eu acho que tá ficando bom

--- lista-compras-na-loja.php
+++ lista-compras-na-loja.php
@@ -18,32 +18,70 @@
 			}
 			
 			h2 {
-				margin: center;
+				margin: auto;
+			}
+			
+			img {
+				width: 300px;
+				height: 170px;
+				border: 5px ridge gray;
+				margin-bottom: 7px;
+				object-fit: cover;
+			}
+			
+			#vc-sel {
+				background: #FFFEFE;
+				width: 36%;
+				height: 65px;
+				overflow-y: auto;
+				overflow-x: auto;
+				margin: auto;
+				border: 5px ridge #7B6C6C;
+				padding: 1px;
+				text-shadow: 2px 2px #ff0000;
+				font-size: 2em;
 			}
 			
 			.produtos {
-				background: rgb(230, 230, 230);
-				width: 91%;
+				background: #FFFEFE;
+				width: 99%;
 				height: 180px;
 				overflow-y: auto;
-				overflow-x: hidden;
-				margin: 17px 0px 19px 6px;
-				border: #FFD4D4;
-				padding: 6px;
+				overflow-x: auto;
+				margin: 17px auto 17px auto;
+				border:  5px  #7B6C6C;
+				padding: 17px;
+				display: flex;
+				justify-content: space-between;
 			}
 			
-			
-			
+			.info-produto {
+				font-family: fantasy;
+				font-size: 20px;
+				margin: center;
+			}
+						
 		</style>
 	</head>
 	<body>
 		<?php
 		include('includes/header.php');
 		?>
-		
-		<h2>Você selecionou:</h2>
+		<div id="vc-sel">
+			<h2>
+				Você selecionou:
+			</h2>
+		</div>
 		<div class="produtos">
-		<p></p>
+			<div class="info-produto">
+				<h2>
+					Exemplo de Produto
+				</h2>
+				<p>
+					A descrição do produto
+				</p>
+			</div>	
+			<img src="assets/img/gif-bye-Groot.gif" alt="A imagem de um produto">
 		</div>
 	</body>
 </html>
