7ba71176ba7765c069086d85ebdd623f04214d9c
Rayssa Fonseca <fonsecarayss14@gmail.com>
2022-05-15 22:27:16 -0300

    adicionei mais produtos na lista

--- banco/insert_produtos.sql
+++ banco/insert_produtos.sql
@@ -5,3 +5,12 @@ insert into mercadoria (nome, marca, slug, descricao) values ('Azeite', 'Gota Da
 insert into mercadoria (nome, marca, slug, descricao) values ('Óleo', 'Puro Óleo', 'oleo-amarelo', 'Ideal para fritar seus pastéis, batatas ou anéis de cebola');
 insert into mercadoria (nome, marca, slug, descricao) values ('Açúcar', 'Bem Formigueiro', 'acucar-refinado', 'O açúcar mais doce que você já viu, mas cuidado com a diabete');
 insert into mercadoria (nome, marca, slug, descricao) values ('Café', 'Cafézin', 'cafe-forte', 'O sabor da fazenda direto para sua casa');
+insert into mercadoria (nome, marca, slug, descricao) values ('Arroz integral', 'pequeno grâo', 'arroz-integral', '1 quilo');
+insert into mercadoria (nome, marca, slug, descricao) values ('Achocolatado', 'Nescau','chocolate-po', 'Energia que dá força');
+insert into mercadoria (nome, marca, slug, descricao) values ('Molho de Tomate', 'Heinz', 'extrato-tomate', 'Compedaços de tomate');
+insert into mercadoria (nome, marca, slug, descricao) values ('Cereal', 'Nestle', 'cereal', 'Cereal matinal tradicional');
+insert into mercadoria (nome, marca, slug, descricao) values ('Milho Verde', 'Quero', 'milho-enlatado', 'Milho Verde pronto para servir');
+insert into mercadoria (nome, marca, slug, descricao) values ('Ervilha', 'Quero', 'ervilha-enlatada', 'Ervilha pronta para servir');
+insert into mercadoria (nome, marca, slug, descricao) values ('Biscoito', 'É bolacha!', 'biscoito-morango', 'Biscoito de chocolate com recheio de morango');
+insert into mercadoria (nome, marca, slug, descricao) values ('Biscoito', 'Nesfit', 'biscoito-salgado', 'Biscoito integral com gergelim');
+    
