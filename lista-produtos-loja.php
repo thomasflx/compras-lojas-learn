3f15e65d20e16f15f98dd705f504b84f8801772c
Abner <abnerm.arruda@gmail.com>
2022-09-22 20:01:18 -0300

    tentei colocar uma class e img n vi se deu certo, seja o que DEUS quiser.

--- lista-produtos-loja.php
+++ lista-produtos-loja.php
@@ -1,5 +1,8 @@
 <?php
 
+include('includes/init.php');
+include('includes/db.php');
+
 // Define as variáveis
 $loja = null;
 $setor_chave = null;
@@ -13,81 +16,7 @@ if (!empty($_GET['setor'])) {
 }
 
 // todo: Conectar e buscar dados do banco - Abner
-// todo: utilizar o include do arquivo db.php
-
-// todo: Adicionar a coluna de imagem no banco
-// todo: Incluir o arquivo init.php e utilizar a variável $siteurl
-
-// Todos os setores e seus produtos
-$setores = array(
-	'mercearia' => [
-		'titulo' => 'Mercearia',
-		'produtos' => [
-			'arroz' => 'Arroz',
-			'acucar' => 'Açucar',
-			'bolachas' => 'Bolachas',
-			'salgadinho' => 'Salgadinho', 
-			'cafe-soluvel' => 'Café Solúvel',
-			'cha' => 'Chá',
-			'macarrao' => 'Macarrão'
-		]
-	],
-	'limpeza' => [
-		'titulo' => 'Produtos de limpeza',
-		'produtos' => [
-			'candida' => 'Cândida',
-			'ajax' => 'Ajax',
-			'detergente' => 'Detergente'
-		]
-	],
-	'padaria' => [
-		'titulo' => 'Padaria Sonho de Padeiro',
-		'produtos' => [
-			'pao-frances' => 'Pão Francês',
-			'pao-doce' => 'Pão doce',
-			'pao-queijo' => 'Pão de queijo'
-		]
-	],
-	'perfumaria' => [
-		'titulo' => 'Perfumaria',
-		'produtos' => [
-			'shampoo' => 'Shampoo',
-			'creme-hidrantante' => 'Creme Hidratante',
-			'algodao' => 'Algodão',
-			'creme-dental' => 'Creme Dental',
-			'esmalte' => 'Esmalte'
-		]
-	],
-	'acougue' => [
-		'titulo' => 'Açougue',
-		'produtos' => [
-			'aves' => 'Aves',
-			'bovinas' => 'Bovínas',
-			'suinas' => 'Suínas'
-		]
-	],
-	'hortifrut' => [
-		'titulo' => 'Coisa de Feira',
-		'produtos' => [
-			'maca-verde' => 'Maça Verde',
-			'banana' => 'Banana',
-			'abobora' => 'Abóbora',
-			'batata-doce' => 'Batata Doce',
-			'alface' => 'Alface'
-		]
-	],
-	'bebidas' => [
-		'titulo' => 'Bebidas Prontas',
-		'produtos' => [
-			'agua-mineral' => 'Água Mineral',
-			'agua-gas' => 'Água com Gás',
-			'refri' => 'Refrigerante',
-			'vinho' => 'Vinho'
-		]
-	]
-);
-
-?>
+// todo: Utilizar a variável $siteurl
 
 <!DOCTYPE html>
 
@@ -98,8 +27,12 @@ $setores = array(
 	include('includes/head.php');
 	?>
 	
+
 	<!-- todo: deixar a página tinindo kkkk-->
 	<style>
+		.img-prod {
+			
+		}
 	</style>
 </head>
 
@@ -116,6 +49,9 @@ $setores = array(
 			<ul>
 				<li><?=$produto?></li>
 			</ul>
+			<?php if (!empty($loja['imagem'])): ?>
+				<img class="img-prod" src="<?=$siteurl . $loja['imagem']?>" alt="Imagem representativa da loja.">
+			<?php endif; ?>
 		<?php endforeach; ?>
 		
 	<?php else : ?>
